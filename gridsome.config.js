f206bcc3b6998dd43d3a02aa99308770c95931bc
Mateusz Jablonowski <mateuszjablonowski@MacBook-Pro-Mateusz.local>
2022-03-01 22:07:41 +0100

    bootstrap fixed

--- gridsome.config.js
+++ gridsome.config.js
@@ -15,8 +15,26 @@ module.exports = {
           },
         }
       }
+    },
+    // {
+    //   use: '@gridsome/plugin-google-analytics',
+    //   options: {
+    //     id: 'UA-72659574-10'
+    //   }
+    // },
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000
+      }
     }
   ],
+  css: {
+    loaderOptions: {
+      scss: {
+      }
+    }
+  },
   templates: {
     Tag: '/tag/:id',
     Post: '/blog/:path',
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
+import BootstrapVue from 'bootstrap-vue'
+import './assets/styles/main.scss'
 import DefaultLayout from '~/layouts/Default.vue'
+import checkIfMobile from './mixins/checkIfMobile'
 
 export default function (Vue, { router, head, isClient }) {
-  // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.use(BootstrapVue);
+  Vue.component('Layout', DefaultLayout);
+  Vue.mixin(checkIfMobile);
 }
